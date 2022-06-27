4d65e342c0bf07a171759d96a6bf5aa30642bee3
miju12 <mouryabaruah@gmail.com>
2022-06-27 15:56:44 +0000

    candy-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_sweet.mk
+    $(LOCAL_DIR)/candy_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_sweet-eng \
-    awaken_sweet-userdebug \
-    awaken_sweet-user
+    candy_sweet-eng \
+    candy_sweet-userdebug \
+    candy_sweet-user
